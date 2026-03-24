ce33e8ab728487dd5b078c5bcb1550d1613fa2f3
Johan Jibu Varghese <johanjjv04@gmail.com>
2026-03-25 00:26:58 +0530

    Keep only tic-tac-toe.c in the repository

--- ttt/ai.h
+++
@@ -1,6 +0,0 @@
-#ifndef AI_H
-#define AI_H
-
-int ai_choose_move(char ai_p, char human_p);
-
-#endif
--- ttt/game.h
+++
@@ -1,7 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-
-void run_archivist_pvp();
-void run_archivist_ai();
-
-#endif
